1386dccaf8aecd510974c4eb9630be16cdf53c66
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-03-15 19:39:28 +0300

    fix: remove console.log

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
   bracketsConfig = bracketsConfig.map((item, index) => item = item.join(''));
-console.log(bracketsConfig+' '+str);
 if (str.length % 2 == 0) {
   for (let i = 0; i<bracketsConfig.length; i++){
     if (str.includes(bracketsConfig[i])) {
       str = str.replace(bracketsConfig[i], '');
       i=i-3;
-      console.log(str);
     }
   }
   if (str==='') {
